f8dd80be74cb7ead9173699114edd0d9f8fb950c
yxhwxn <officeyh9819@gmail.com>
2024-11-01 22:42:56 +0900

    Init project

---
+++ src/test/java/unid/team4/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package unid.team4.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
