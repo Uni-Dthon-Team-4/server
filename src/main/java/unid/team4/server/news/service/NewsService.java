d1a529053b182a45420fe15801d2f38a6abd6db9
yxhwxn <officeyh9819@gmail.com>
2024-11-02 23:16:06 +0900

    Feat: 1인가구 관련 뉴스 조회 API 구현

--- src/main/java/unid/team4/server/news/service/NewsService.java
+++ src/main/java/unid/team4/server/news/service/NewsService.java
@@ -1,4 +1,29 @@
 package unid.team4.server.news.service;
 
+import org.springframework.stereotype.Service;
+import unid.team4.server.news.controller.dto.NewsResponseDTO;
+import unid.team4.server.news.domain.News;
+import unid.team4.server.news.domain.repository.NewsRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class NewsService {
+    private final NewsRepository newsRepository;
+
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public List<NewsResponseDTO> getAllNews() {
+        List<News> newsList = newsRepository.findAll();
+        return newsList.stream()
+                .map(news -> new NewsResponseDTO(
+                        news.getNewsId(),
+                        news.getHeadline(),
+                        news.getUrl(),
+                        news.getPreview()))
+                .collect(Collectors.toList());
+    }
 }
